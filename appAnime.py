d1ebde6ae01eb213c5fe30889b5524ed47b9765d
Mohamed Abdirizak <73124836+MKVde@users.noreply.github.com>
2023-05-10 21:16:43 +0300

    Update appAnime.py

--- appAnime.py
+++ appAnime.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import streamlit as st
+import time
 
 # Define the CSP
 csp = """
@@ -38,6 +39,7 @@ def run_script(url, provider):
 
             # Make a GET request to the episode page
             try:
+                time.sleep(10)
                 response = requests.get(episode_url)
                 response.raise_for_status()
             except requests.exceptions.RequestException:
