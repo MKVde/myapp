7b7c28144c395a0c78ac0d15707beaff60614223
Mohamed Abdirizak <73124836+MKVde@users.noreply.github.com>
2023-05-10 18:57:02 +0300

    Update appAnime.py

--- appAnime.py
+++ appAnime.py
@@ -1,36 +1,27 @@
-import time
+import requests
+import cfscrape
+from bs4 import BeautifulSoup
 import streamlit as st
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
-import undetected_chromedriver.v2 as uc
-
 
 def run_script(url, provider):
     # Check that URL is valid
     try:
-        driver.get(url)
-    except TimeoutException:
+        scraper = cfscrape.create_scraper() # Use cfscrape to bypass Cloudflare
+        response = scraper.get(url)
+        response.raise_for_status()
+        print(f"Status Code: {response.status_code}") # Print the status code
+        st.warning(f"Status Code: {response.status_code}") # Print the status code
+    except requests.exceptions.RequestException:
         st.warning("No URL or Invalid URL. Please enter a valid URL.")
         return
 
     # Show loading spinner
     with st.spinner(text="Loading..."):
+        # Use BeautifulSoup to parse the response HTML
+        soup = BeautifulSoup(response.text, "html.parser")
+
         # Find all the episode links on the page
-        try:
-            episode_links = WebDriverWait(driver, 10).until(
-                EC.presence_of_all_elements_located(
-                    (By.CSS_SELECTOR, ".episodes-card-title a")
-                )
-            )
-        except TimeoutException:
-            st.warning("Failed to retrieve episode data. Please check your internet connection and try again.")
-            return
+        episode_links = soup.select(".episodes-card-title a")
 
         # Extract anime name from URL
         anime_name = url.split('/')[-2].replace('-', ' ')
@@ -43,29 +34,25 @@ def run_script(url, provider):
 
         # Loop through each episode URL
         for episode_link in episode_links:
-            episode_url = episode_link.get_attribute("href")
+            episode_url = episode_link["href"]
 
             # Make a GET request to the episode page
             try:
-                driver.get(episode_url)
-            except TimeoutException:
+                response = scraper.get(episode_url)
+                response.raise_for_status()
+            except requests.exceptions.RequestException:
                 st.warning("Failed to retrieve episode data. Please check your internet connection and try again.")
                 return
 
+            # Parse the HTML using BeautifulSoup
+            soup = BeautifulSoup(response.text, 'html.parser')
+
             # Find all HTML elements containing the provider link
-            try:
-                provider_links = WebDriverWait(driver, 10).until(
-                    EC.presence_of_all_elements_located(
-                        (By.XPATH, f"//a[contains(@href,'{provider}')]")
-                    )
-                )
-            except TimeoutException:
-                st.warning("Failed to retrieve episode data. Please check your internet connection and try again.")
-                return
+            provider_links = soup.find_all('a', href=lambda href: href and provider in href)
 
             # Extract the second link from the list of provider links
             if len(provider_links) >= 2:
-                download_link = provider_links[1].get_attribute("href")
+                download_link = provider_links[1]['href']
                 episode_num = episode_link.text.split(' ')[-1]
                 
                 # Add the row to the table
@@ -81,17 +68,4 @@ provider = st.selectbox("Provider:", ["mega", "google", "mediafire"])
 
 # Run the script when the "Download" button is clicked
 if st.button("Run"):
-    # Set up undetected ChromeDriver
-    options = uc.ChromeOptions()
-    options.add_argument("--headless")
-    options.add_argument("--disable-gpu")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
-    driver = uc.Chrome(options=options)
-    driver.delete_all_cookies()
-
-    # Run the script
     run_script(url, provider)
-
-    # Quit the driver
-    driver.quit()
